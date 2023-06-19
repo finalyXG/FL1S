7f0f73299d9f6118d9f334c4a6ec369aada312d8
diya <diya.dai@fcc-analytics.com>
2023-06-19 11:09:47 +0800

    class ExampleModel build_model func - rm loss, acc\ change layers

--- models/example_model.py
+++ models/example_model.py
@@ -22,15 +22,13 @@ class ExampleModel(Model):
         return self.d2(x)
 
 
-        with tf.name_scope("loss"):
-            self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=d2))
-            update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-            with tf.control_dependencies(update_ops):
-                self.train_step = tf.train.AdamOptimizer(self.config.learning_rate).minimize(self.cross_entropy,
-                                                                                         global_step=self.global_step_tensor)
-            correct_prediction = tf.equal(tf.argmax(d2, 1), tf.argmax(self.y, 1))
-            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+    def build_model(self):
+        self.conv1 = Conv2D(32, 3, activation='relu')
+        self.flatten = Flatten()
+        self.d1 = Dense(128, activation='relu')
+        self.d2 = Dense(10)
+    
 
     def init_saver(self):
         # here you initialize the tensorflow saver that will be used in saving the checkpoints.
