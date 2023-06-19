18c785e6b115f78e8530e3a76f54e6a6d8c0f3ff
diya <diya.dai@fcc-analytics.com>
2023-06-19 11:43:28 +0800

    class train_epoch train_step func - rewrite with V2.0

--- trainers/example_trainer.py
+++ trainers/example_trainer.py
@@ -32,9 +32,27 @@ class ExampleTrainer(BaseTrain):
         # self.logger.summarize(cur_it, summaries_dict=summaries_dict)
         self.model.save()
 
+    # def train_step(self):
+    #     batch_x, batch_y = next(self.data.next_batch(self.config.batch_size))
+    #     feed_dict = {self.model.x: batch_x, self.model.y: batch_y, self.model.is_training: True}
+        
+    #     _, loss, acc = self.sess.run([self.model.train_step, self.model.cross_entropy, self.model.accuracy],
+    #                                  feed_dict=feed_dict)
+    #     return loss, acc
+    
+    @tf.function
     def train_step(self):
-        batch_x, batch_y = next(self.data.next_batch(self.config.batch_size))
-        feed_dict = {self.model.x: batch_x, self.model.y: batch_y, self.model.is_training: True}
-        _, loss, acc = self.sess.run([self.model.train_step, self.model.cross_entropy, self.model.accuracy],
-                                     feed_dict=feed_dict)
-        return loss, acc
+        images, labels = next(self.data.next_batch(self.config.batch_size))
+    
+        loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+        with tf.GradientTape() as tape:
+            # training=True is only needed if there are layers with different
+            # behavior during training versus inference (e.g. Dropout).
+            predictions = self.model(images, training=True)
+            loss = loss_object(labels, predictions)
+
+        gradients = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+
+        return self.train_loss(loss), self.train_accuracy(labels, predictions)
