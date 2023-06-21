31d6e3a111cfd2c777f168f588cdbbedb1ff0c96
diya <diya.dai@fcc-analytics.com>
2023-06-21 12:21:17 +0800

    replace class ExampleModel class with BaseModel

--- models/example_model.py
+++ models/example_model.py
@@ -1,40 +1,22 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras.layers import Dense, Flatten,LeakyReLU, Conv2DTranspose,Dropout, Conv2D, Reshape, BatchNormalization
 from tensorflow.keras import Model
-class ExampleModel(Model):
+
+class BaseModel(Model):
     def __init__(self, config):
-        super(ExampleModel, self).__init__(config)
+        super(BaseModel, self).__init__()
         self.config = config
         # init the global step
         self.init_global_step()
         # init the epoch counter
         self.init_cur_epoch()
  
-        self.build_model()
+        # self.build_model()
         self.init_saver()
-
-
-
-    def call(self, x):
-        x = self.conv1(tf.expand_dims(x, axis=-1))
-        x = self.flatten(x)
-        x = self.d1(x)
-        return self.d2(x)
-
-
-
-    def build_model(self):
-        self.conv1 = Conv2D(32, 3, activation='relu')
-        self.flatten = Flatten()
-        self.d1 = Dense(128, activation='relu')
-        self.d2 = Dense(10)
-    
 
     def init_saver(self):
         # here you initialize the tensorflow saver that will be used in saving the checkpoints.
         self.saver = tf.train.Checkpoint(max_to_keep=tf.Variable(self.config.max_to_keep , dtype=tf.int64)) 
-
-
         # save function that saves the checkpoint in the path defined in the config file
     def save(self):
         print("Saving model...")
