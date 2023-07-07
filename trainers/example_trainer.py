9323efc4336eaa438d994d56b7bb9336aa1d23f7
diya <diya.dai@fcc-analytics.com>
2023-07-07 18:18:40 +0800

    class Trainer - tensorboard visualize related

--- trainers/example_trainer.py
+++ trainers/example_trainer.py
@@ -1,4 +1,7 @@
 from tqdm import tqdm
+import datetime
+import io
+import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.pyplot as plt
 from IPython import display
@@ -12,15 +15,30 @@ class Trainer:
         self.config = config
         self.data = data
 
-class Trainer(BaseTrain):
-    def __init__(self, data, discriminator, generator, latent_dim, config):#,logger):
-        super(Trainer, self).__init__(data, discriminator, generator, config)#,logger)
+        self.img_save_path = "./generate_img"
         self.latent_dim = latent_dim
         self.seed = tf.random.normal([self.config.num_examples_to_generate, self.latent_dim])
-
+        
+        self.loss_fn = tf.keras.losses.BinaryCrossentropy(from_logits=False)     
         self.disc_optimizer = tf.keras.optimizers.legacy.Adam(self.config.learning_rate)
         self.gen_optimizer = tf.keras.optimizers.legacy.Adam(self.config.learning_rate)
 
+        self.disc_train_loss = tf.keras.metrics.Mean(name='disc_train_loss')
+        self.gen_train_loss = tf.keras.metrics.Mean(name='gen_train_loss')  
+
+        self.disc_test_loss = tf.keras.metrics.Mean(name='disc_test_loss')
+        self.gen_test_loss = tf.keras.metrics.Mean(name='gen_test_loss')
+
+        self.disc_train_acc =  tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+        self.disc_test_acc =  tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+        
+        current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        train_log_dir = 'logs/gradient_tape/' + current_time + '/train'
+        test_log_dir = 'logs/gradient_tape/' + current_time + '/test'
+        logdir = "logs/train_data/" + current_time
+        self.train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+        self.test_summary_writer = tf.summary.create_file_writer(test_log_dir)
+        self.file_writer = tf.summary.create_file_writer(logdir)
 
     def __call__(self):
         if self.init is None:
@@ -34,29 +52,62 @@ class Trainer(BaseTrain):
                                         discriminator_optimizer=self.gen_optimizer,
                                         generator=self.generator,
                                         discriminator=self.discriminator)
-        
-        for cur_epoch in range(self.generator.cur_epoch_tensor.numpy(), self.config.num_epochs + 1, 1):#range(self.model.cur_epoch_tensor.eval(self.sess), self.config.num_epochs + 1, 1):
-            self.train_epoch()
+
+        for cur_epoch in range(self.generator.cur_epoch_tensor.numpy(), self.config.num_epochs + 1, 1):
+            for _ in range(self.config.num_iter_per_epoch):
+                self.train_step()
+            with self.train_summary_writer.as_default():
+                tf.summary.scalar('disc_loss', self.disc_train_loss.result(), step=cur_epoch)
+                tf.summary.scalar('gen_loss', self.gen_train_loss.result(), step=cur_epoch)
+                tf.summary.scalar('disc_acc', self.disc_train_acc.result(), step=cur_epoch)
+
             self.generator.cur_epoch_tensor.assign_add(1)
             self.discriminator.cur_epoch_tensor.assign_add(1)
             display.clear_output(wait=True)
             print('cur_epoch',cur_epoch+1)
-            self.generate_and_save_images(self.generator,
+
+            img = self.generate_and_save_images(self.generator,
                             cur_epoch + 1,
                             self.seed)
+            # Convert to image and log
+            with self.file_writer.as_default():
+                tf.summary.image("Generate data", img, step=cur_epoch)
+            
             for(X_test, Y_test) in self.data.test_batched:
                 self.test_step(X_test, Y_test)
+            with self.test_summary_writer.as_default():
+                tf.summary.scalar('disc_loss', self.disc_test_loss.result(), step=cur_epoch)
+                tf.summary.scalar('gen_loss', self.gen_test_loss.result(), step=cur_epoch)
+                tf.summary.scalar('disc_acc', self.disc_test_acc.result(), step=cur_epoch)
+            template = 'Epoch {}, Generator Loss: {}, Discriminator Loss: {}, Discriminator Accuracy: {}, Generator Test Loss: {}, Discriminator Test Loss: {}, Discriminator Test Accuracy: {}'
+            print (template.format(cur_epoch+1,
+                                    self.gen_train_loss.result(), 
+                                    self.disc_train_loss.result(), 
+                                    self.disc_train_acc.result()*100,
+                                    self.gen_test_loss.result(), 
+                                    self.disc_test_loss.result(), 
+                                    self.disc_test_acc.result()*100))
+
+            # Reset metrics every epoch
+            self.gen_train_loss.reset_states()
+            self.disc_train_loss.reset_states()
+            self.gen_test_loss.reset_states()
+            self.disc_test_loss.reset_states()
+            self.disc_train_acc.reset_states()
+            self.disc_test_acc.reset_states()
             # Save the model every 15 epochs
-            if (cur_epoch + 1) % 15 == 0:
-                checkpoint.save(file_prefix = checkpoint_prefix)
+            # if (cur_epoch + 1) % 15 == 0:
+            #     checkpoint.save(file_prefix = checkpoint_prefix)
 
          # Generate after the final epoch
         display.clear_output(wait=True)
-        self.generate_and_save_images(self.generator,
+        img = self.generate_and_save_images(self.generator,
                                 self.config.num_epochs + 1,
                                 self.seed)
         
-
+        with self.file_writer.as_default():
+            tf.summary.image("Generate data", img, step=self.config.num_epochs + 1)
+        
     def discriminator_loss(self, real_output, fake_output):
         real_loss = self.loss_fn(tf.ones_like(real_output), real_output)
         fake_loss = self.loss_fn(tf.zeros_like(fake_output), fake_output)
@@ -66,27 +117,13 @@ class Trainer(BaseTrain):
     def generator_loss(self, fake_output):
         return self.loss_fn(tf.ones_like(fake_output), fake_output)
 
-    def train_epoch(self):
-        loop = tqdm(range(self.config.num_iter_per_epoch))
-        gen_losss = []
-        disc_losss = []
-        for _ in loop:
-            gen_loss, disc_loss = self.train_step()
-            gen_losss.append(gen_loss)
-            disc_losss.append(disc_loss)
-        gen_loss = np.mean(gen_losss)
-        disc_loss = np.mean(disc_losss)
-
-        print("gen_loss::::",gen_loss)
-        print("disc_loss:::",disc_loss)
-        # self.logger.summarize(cur_it, summaries_dict=summaries_dict)
-        self.discriminator.save()
-        self.generator.save()
 
     def generate_and_save_images(self,model, epoch, test_input):
         # Notice `training` is set to False.
         # This is so all layers run in inference mode (batchnorm).
         predictions = model(test_input, training=False)
+        # Save the plot to a PNG in memory.
+        buf = io.BytesIO()
 
         fig = plt.figure(figsize=(4, 4))
 
@@ -95,9 +132,14 @@ class Trainer(BaseTrain):
             plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
             plt.axis('off')
 
-        plt.savefig('./generate_img/image_at_epoch_{:04d}.png'.format(epoch))
+        plt.savefig(buf, format='png')
         plt.close()
-        # plt.show()
+        buf.seek(0)
+        # Convert PNG buffer to TF image
+        image = tf.image.decode_png(buf.getvalue(), channels=4)
+        # Add the batch dimension
+        image = tf.expand_dims(image, 0)
+        return image
     
     # @tf.function: The below function is completely Tensor Code
     # Good for optimization
@@ -123,6 +165,12 @@ class Trainer(BaseTrain):
         self.disc_optimizer.apply_gradients(zip(grad_disc, self.discriminator.trainable_variables))
         self.gen_optimizer.apply_gradients(zip(grad_gen, self.generator.trainable_variables))
         
+        #produce disc labels and output
+        labels = tf.concat([tf.ones_like(real_output), tf.zeros_like(fake_output)], 0)
+        output = tf.concat([real_output ,fake_output],0)
+   
+        return self.gen_train_loss(gen_loss), self.disc_train_loss(disc_loss),self.disc_train_acc(labels,output)
+
     @tf.function
     def test_step(self,images, labels):
         noise = tf.random.normal([self.config.batch_size, self.latent_dim])
