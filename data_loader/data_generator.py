11d5130ee3250351df2876a9a0153f360fe5ee26
diya <diya.dai@fcc-analytics.com>
2023-07-07 18:20:50 +0800

    class DataGenerator - preprocess test dataset

--- data_loader/data_generator.py
+++ data_loader/data_generator.py
@@ -1,20 +1,22 @@
 import numpy as np
 import tensorflow as tf
+import random
 
 class DataGenerator:
     def __init__(self, config):
         self.config = config
-        # load data here
-
-        # self.input = np.ones((500, 784))
-        # self.y = np.ones((500, 10))
-
         mnist = tf.keras.datasets.mnist
 
-        (self.input, self.y), (_, __) = mnist.load_data()
+        (self.input, self.y), (self.test_x, self.test_y) = mnist.load_data()
         self.input = self.input.reshape(self.input.shape[0], 28, 28, 1).astype('float32')
         self.input = (self.input - 127.5) / 127.5  # Normalize the images to [-1, 1]
 
+        self.test_x = self.test_x.reshape(self.test_x.shape[0], 28, 28, 1).astype('float32')
+        self.test_x = (self.test_x - 127.5) / 127.5  # Normalize the images to [-1, 1]
+
+        self.test_batched = tf.data.Dataset.from_tensor_slices((self.test_x, self.test_y)).batch(self.config.batch_size)
+
+
     def next_batch(self, batch_size):
-        idx = np.random.choice(500, batch_size)
+        idx = np.random.choice(len(self.y), batch_size)
         yield self.input[idx], self.y[idx]
