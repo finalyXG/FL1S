3695889edab364ff4138e230c2450ba48c0c2a68
diya <diya.dai@fcc-analytics.com>
2023-07-19 09:50:29 +0800

    class DataGenerator - modify y label to one hot format

--- data_loader/data_generator.py
+++ data_loader/data_generator.py
@@ -7,13 +7,16 @@ class DataGenerator:
         self.config = config
         mnist = tf.keras.datasets.mnist
 
-        (self.input, self.y), (self.test_x, self.test_y) = mnist.load_data()
+        (self.input, self._y), (self.test_x, self._test_y) = mnist.load_data()
         self.input = self.input.reshape(self.input.shape[0], 28, 28, 1).astype('float32')
         self.input = (self.input - 127.5) / 127.5  # Normalize the images to [-1, 1]
 
         self.test_x = self.test_x.reshape(self.test_x.shape[0], 28, 28, 1).astype('float32')
         self.test_x = (self.test_x - 127.5) / 127.5  # Normalize the images to [-1, 1]
-        
+        # change y lable to one_hot
+        self.y = tf.keras.utils.to_categorical(self._y, config.num_classes)
+        self.test_y = tf.keras.utils.to_categorical(self._test_y, config.num_classes)
+
         self.test_batched = tf.data.Dataset.from_tensor_slices((self.test_x[:self.config.test_sample_num], self.test_y[:self.config.test_sample_num])).shuffle(10000).batch(self.config.batch_size,drop_remainder=True)
         # self.train_batched = tf.data.Dataset.from_tensor_slices(
         #     (self.input[:self.config.train_sample_num] , self.y[:self.config.train_sample_num])).shuffle(50000).batch(self.config.batch_size,drop_remainder=True)
