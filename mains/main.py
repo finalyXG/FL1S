b420ce91c61c9635c46c192d2d51a053c507fde1
diya <diya.dai@fcc-analytics.com>
2023-08-10 09:33:11 +0800

    6. main - refactor: load config value from main func rather than json file

--- mains/main.py
+++ mains/main.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from data_loader.data_generator import DataGenerator
 from trainers.example_trainer import Trainer
+from models.example_model import Classifier
 from utils.config import process_config
 from utils.dirs import create_dirs
 from utils.utils import get_args
@@ -8,16 +9,14 @@ from tensorboard.plugins.hparams import api as hp
 from sklearn.manifold import TSNE
 import seaborn as sns
 import matplotlib.pyplot as plt
-import random
 from client import clients_main
 import numpy as np
 import pandas as pd
-def main():
-    # capture the config path from the run arguments
-    # then process the json configuration file
-    try:
-        args = get_args()
-        config = process_config(args.config)
+import argparse
+import os
+import time
+import pickle
+import random
 
     except:
         print("missing or invalid arguments")
@@ -63,4 +62,31 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    # General command line arguments for all models
+    parser.add_argument("--test_feature_num", type=int, default=500)
+    parser.add_argument("--client_train_num", type=int, default=2000)
+    parser.add_argument("--client_test_num", type=int, default=2000)
+    parser.add_argument("--features_ouput_layer", help="The index of features output Dense layer",type=int, default=2)
+    parser.add_argument("--image_size", type=int, default=28)
+    parser.add_argument("--buffer_size", type=int, default=5000)
+    parser.add_argument("--latent_dim", type=int, default=16)
+    # parser.add_argument("--feature_dim", type=int, default=16)
+    parser.add_argument("--max_to_keep", type=int, default=5)
+    parser.add_argument("--num_classes", type=int, default=10)
+
+    parser.add_argument("--num_clients", type=int, default=2)
+    parser.add_argument("--random_seed", type=int, default=10)
+
+    parser.add_argument("--exp_name", type=str, default="example")
+    parser.add_argument("--logdir", type=str, default="logs/hparam_tuning")
+
+    args = parser.parse_args()
+    tf.random.set_seed(args.random_seed)
+    np.random.seed(args.random_seed)
+    random.seed(args.random_seed)
+
+    print("num_clients:", args.num_clients)
+    print("test_feature_num:", args.test_feature_num)
+    main(args)   #use model generate feature
+    # use_npy_generate_feature(args)
