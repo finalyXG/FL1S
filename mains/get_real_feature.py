4003b5398731f8056b4ad2720e4b06cba2eae9e6
diya <diya.dai@fcc-analytics.com>
2023-10-04 18:11:46 +0800

    feat: produce real feature independently by use model checkpoint

---
+++ mains/get_real_feature.py
@@ -0,0 +1,162 @@
+import tensorflow as tf
+from models.example_model import Classifier, ClassifierElliptic, C_Discriminator,C_Generator, AC_Discriminator, AC_Generator
+from data_loader.data_generator import DataGenerator
+from tensorboard.plugins.hparams import api as hp
+import numpy as np
+import os
+import argparse
+import pickle
+import random
+
+def main(config):
+    # hparams_file = open('/hparams_record.txt', "r")
+    # print(hparams_file.readlines())
+    # config_dict = vars(config)
+    # for line in hparams_file.readlines():
+    #     line = line.strip()
+    #     k, v = line.split(": ")[0], line.split(": ")[1]
+
+    #     print("v type", type(v))
+    #     config_dict[k] = v
+    data = DataGenerator(config)
+    client_data = data.clients[config.clients_name]
+
+    if config.dataset != "elliptic":
+        cls = Classifier(config)
+    else:
+        cls = ClassifierElliptic(config)
+    path = f'tmp/{config.clients_name}/{config.version_num}/'
+    cls.load_weights(tf.train.latest_checkpoint(f'{path}/cls_training_checkpoints/local/')).expect_partial()
+
+    (train_data, test_data) = client_data
+    train_x,train_y = zip(*train_data)
+    train_x,train_y = np.array(train_x),np.array(train_y)
+
+    test_x,test_y = zip(*test_data)
+    test_x,test_y = np.array(test_x),np.array(test_y)
+
+    real_train_features = cls.get_features(train_x)
+    real_test_features = cls.get_features(test_x)
+    if not os.path.exists(f"{path}/save/"):
+        os.makedirs(f"{path}/save/")
+    np.save(f"{path}/save/real_train_features",real_train_features)
+    np.save(f"{path}/save/train_y",train_y)
+
+    np.save(f"{path}/save/real_test_features",real_test_features)
+    np.save(f"{path}/save/test_y",test_y)
+    # pre_feature  = np.load(f"{path}/real_features.npy",allow_pickle=True)
+    # pre_label  = np.load(f"{path}/features_label.npy",allow_pickle=True)
+    # print("pre_feature",pre_feature[0][:3])
+    # print("pre_label",pre_label)
+    # print("-------")
+    # print("real_features",real_train_features[0][:3])
+    # print("train_y",train_y)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # General command line arguments for all models
+    parser.add_argument("--version_num", type=int, default=0)
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="mnist"
+    )
+    parser.add_argument(
+        "--clients_name",
+        type=str,
+        help="Name of client",
+        default="clients_1",
+    )
+    parser.add_argument(
+        "--sample_ratio",
+        type=float,
+        help="Fraction of training data to make available to users",
+        default=1,
+    )
+    parser.add_argument(
+        "--take_feature_ratio",
+        type=float,
+        help="Fraction of pre-feature data to make available to users",
+        default=1,
+    )
+    parser.add_argument(
+        "--alpha",
+        type=float,
+        help="Measure of heterogeneity (higher is more homogeneous, lower is more heterogenous)",
+        default=10,
+    )
+    parser.add_argument("--initial_client", type=int, default=1)  #use 0 and 1 to replace False and True 
+    parser.add_argument(
+        "--initial_client_ouput_feat_epochs", 
+        type=int, 
+        nargs='+', 
+        help="Define generate trained feature data in which epoch",
+        default=[-1]
+        ) 
+    parser.add_argument(
+        "--whether_initial_feature_center", 
+        type=int, 
+        help="Whether send random initial feature center to initial client",
+        default=0
+        ) 
+    parser.add_argument(
+        "--initial_feature_center_cosine_threshold",
+        type=float, 
+        help="Define the consine similarity socre each initial feature center have to reach",
+        default=0.5
+    )
+    parser.add_argument("--features_central_client_name_list", type=str, nargs='+', default=["clients_1"])  #use which version as initial client( only for initial_client==0)
+    parser.add_argument("--features_central_version_list", type=str,nargs='+',  default=["0"])  #use which version as initial client( only for initial_client==0)
+    parser.add_argument("--use_initial_model_weight", type=int, default=0)  #use which version as initial client( only for initial_client==0)
+    parser.add_argument("--use_assigned_epoch_feature", type=int, default=0)  #use 0 means False==> use feature in best local acc( only for initial_client==0)
+    parser.add_argument("--use_dirichlet_split_data", type=int, default=1)  #use 0 means False, 1 means True
+    parser.add_argument("--use_same_kernel_initializer", type=int, default=1)
+    parser.add_argument("--feature_match_train_data", type=int, default=1)  #1 means set the length of feature to be the same as the length of train data, 0 reverse
+    parser.add_argument("--update_feature_by_epoch", type=int, default=0)
+    parser.add_argument("--cls_num_epochs", type=int, default=20)
+
+    parser.add_argument("--original_cls_loss_weight_list", type=float, nargs='+', default=[1.0])
+    parser.add_argument("--feat_loss_weight_list", type=float, nargs='+', default=[1.0])
+    parser.add_argument("--cos_loss_weight_list", type=float, nargs='+', default=[5.0]) 
+    parser.add_argument("--learning_rate_list", type=float, nargs='+', default=[0.001]) 
+    parser.add_argument("--batch_size_list", type=int, nargs='+', default=[32]) 
+
+    parser.add_argument("--features_ouput_layer", help="The index of features output Dense layer",type=int, default=-2)
+    parser.add_argument("--GAN_num_epochs", type=int, default=1)
+    parser.add_argument("--test_feature_num", type=int, default=500)
+    parser.add_argument("--test_sample_num", help="The number of real features and fake features in tsne img", type=int, default=500) 
+    parser.add_argument("--gp_weight", type=int, default=10.0)
+    parser.add_argument("--discriminator_extra_steps", type=int, default=3)
+    parser.add_argument("--num_examples_to_generate", type=int, default=16)
+    parser.add_argument("--latent_dim", type=int, default=16)
+    parser.add_argument("--feature_dim", type=int, default=128)
+    parser.add_argument("--max_to_keep", type=int, default=5)
+
+    parser.add_argument("--num_clients", type=int, default=2)
+    parser.add_argument("--client_train_num", type=int, default=1000)
+    parser.add_argument("--client_test_num", type=int, default=1000)
+    parser.add_argument("--random_seed", type=int, default=10)
+    parser.add_argument("--data_random_seed", type=int, default=1693)
+
+    parser.add_argument("--exp_name", type=str, default="example")
+    parser.add_argument("--logdir", type=str, default="logs/hparam_tuning")
+
+    args = parser.parse_args()
+    args.initial_client = bool(args.initial_client)
+    args.use_initial_model_weight = bool(args.use_initial_model_weight)
+    args.use_dirichlet_split_data = bool(args.use_dirichlet_split_data)
+    args.update_feature_by_epoch = bool(args.update_feature_by_epoch)
+    print("client:", args.clients_name)
+    print("Whether initial_client:", args.initial_client)
+    print("features_central_version_list:", args.features_central_version_list)
+    if not args.use_dirichlet_split_data:
+        print("client_train_num:", args.client_train_num)
+        print("client_test_num:", args.client_test_num)
+    print("cls_num_epochs:", args.cls_num_epochs)
+    print("initial_client_ouput_feat_epoch:", args.initial_client_ouput_feat_epochs)
+    print("features_ouput_layer:",args.features_ouput_layer)
+    print("use_dirichlet_split_data",args.use_dirichlet_split_data)
+    if args.initial_client_ouput_feat_epochs[0] <= args.cls_num_epochs:
+        main(args)
+    else:
+        print("initial_client_ouput_feat_epochs must be smaller than cls_num_epochs")
